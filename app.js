9199026e47491b65b883b61979ded937a3212b50
AmulyaGottala <amulya.gottala@gmail.com>
2024-05-06 11:08:34 +0530

    commit

--- app.js
+++ app.js
@@ -5,32 +5,25 @@ function billSplit() {
   const cgst = 0.09;
   const sgst = 0.09;
   const gst = cgst + sgst;
+  let tax = billAmount * gst;
+
   const discount = document.getElementById("checkbox").checked;
 
   if (billAmount <= 0 || isNaN(billAmount)) {
     alert("enter valid amount");
-    // document.getElementById("billAmount").value = "";
-    // document.getElementById("personCount").value = "";
   } else if (personCount <= 1 || isNaN(personCount)) {
     alert("enter more than 1 person to split bill");
-    // document.getElementById("billAmount").value = "";
-    // document.getElementById("personCount").value = "";
   } else if (discount) {
-    let bill = billAmount - billAmount * 0.05 - billAmount * gst;
-    console.log(bill);
-    finalValue = bill / personCount;
-    finalBillAmount.value = Math.round(finalValue);
+    let bill = billAmount - billAmount * 0.05;
+    finalBillAmount.value = Math.round(bill / personCount);
     document.getElementById("billAmount").value = "";
     document.getElementById("personCount").value = "";
     document.getElementById("checkbox").checked = false;
+    document.getElementById("taxAdded").textContent = `Tax Added: ${tax}`;
   } else if (discount === false) {
-    let billing = billAmount - billAmount * gst;
-    console.log(billing);
-    finalValue = billing / personCount;
-    finalBillAmount.value = Math.round(finalValue);
+    finalBillAmount.value = Math.round(billAmount / personCount);
     document.getElementById("billAmount").value = "";
     document.getElementById("personCount").value = "";
+    document.getElementById("taxAdded").textContent = `Tax Added: ${tax}`;
   }
 }
-
-console.log(typeof "sdfjh");
